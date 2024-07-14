c4caa67c5acf9539f14e0db17369d123787b17d3
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2024-07-14 10:07:17 -0300

    mysql adaptation and first migration, aded connection string and crated class salesewbdbcontext

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
