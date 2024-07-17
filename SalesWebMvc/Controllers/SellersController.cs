f8f93f1ef7990c1c9c292a03916b2d9f3f4e2a0a
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2024-07-17 08:51:45 -0300

    SellerService and basic FindAll / Updated buttons

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService ?? throw new ArgumentNullException(nameof(sellerService));
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Seller> sellers = _sellerService.FindAll();
+
+            return View(sellers);
         }
     }
 }
