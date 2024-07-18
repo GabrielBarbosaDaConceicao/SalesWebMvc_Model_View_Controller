574b857cf6880eb5f0cc49bc5920a3774808e3cf
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2024-07-18 08:13:50 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -15,11 +15,32 @@ namespace SalesWebMvc.Controllers
             _sellerService = sellerService ?? throw new ArgumentNullException(nameof(sellerService));
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             List<Seller> sellers = _sellerService.FindAll();
 
             return View(sellers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            if (ModelState.IsValid)
+            {
+                _sellerService.Insert(seller);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
